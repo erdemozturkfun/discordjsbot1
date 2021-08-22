e5e4abce865fb7dab3ad539c3f5ea6da06ef172f
root <root@LAPTOP-2SCA53CF.localdomain>
2021-08-22 10:39:33 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token } = require('./config.json');
+const fs = require('fs');
+const wait = require('util').promisify(setTimeout);
+// require the needed discord.js classes
+const { Client, Intents } = require('discord.js');
+
+// create a new Discord client
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// Place your client and guild ids here
+const clientId = '836878288542105600';
+const guildId = '878617052133326899';
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply({content:"Pong!"});
+		await wait(1000);
+		await interaction.deleteReply();
+	}
+});
+
+client.login(token);
